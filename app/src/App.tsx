2f4078f5497a45ae6fb0ff12b76296201a6f8770
Hamin-Jeon <happak2@@gmail.com>
2022-05-29 12:42:36 +0900

    router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import { Home,SignUp } from './pages';
+// import Home from './pages/Home'
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
   return (
     <>
-      <Home></Home>
-      <SignUp></SignUp>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/signup" element={<SignUp/>} />
+        </Routes>
+      </Router>
+      
     </>
   );
 }
